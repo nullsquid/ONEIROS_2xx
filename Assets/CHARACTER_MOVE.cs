3972ccc0ea8be2ba1305c4a9742e6df99a04bc76
Hyacinth <j.shepard.ramsay@gmail.com>
2015-12-04 19:30:53 -0500

    added grid move--need to constrain bounds

--- Assets/CHARACTER_MOVE.cs
+++ Assets/CHARACTER_MOVE.cs
@@ -3,13 +3,62 @@ using System.Collections;
 
 public class CHARACTER_MOVE : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GFRectGrid grid;
+    public int stepToCycle;
+
+    Transform cachedTransform;
+
+    void Awake()
+    {
+        cachedTransform = transform;
+        if (grid)
+        {
+            grid.AlignTransform(cachedTransform);
+        }
+    }
+
+    void Update()
+    {
+        ButtonPressToMove();
+    }
+    void Move()
+    {
+        
+    }
+
+    void TapToMove()
+    {
+
+    }
+
+    void ButtonPressToMove()
+    {
+        Vector3 curPos;
+        Vector3 newPos;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward);
+            
+            FindNearestFace();
+            
+
+        }
+    }
+
+    Vector3 FindNearestFace()
+    {
+        Vector3 newPosition = grid.WorldToGrid(cachedTransform.position);
+        
+        Debug.Log("yes");
+        newPosition = newPosition + Vector3.forward;
+        
+
+        for(int i = 0; i < 2; i++)
+        {
+            if(Mathf.Abs(newPosition[i]) > grid.size[i]){
+                newPosition[i] -= Mathf.Sign(newPosition[i]) * 2.0f;
+            }
+        }
+        return grid.GridToWorld(newPosition);
+    }
 }
--- Assets/CHAR_GRID_MOVE.cs
+++ Assets/CHAR_GRID_MOVE.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class CHAR_GRID_MOVE : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public GFRectGrid grid;
+	public int unitsToCycles = 1;
+
+	void Move(){
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
