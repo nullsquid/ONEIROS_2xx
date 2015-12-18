cd500a63857479ed8f727d1fcfc94b8c92ebec50
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2015-12-17 22:34:35 -0500

    set up action movement system

---
+++ Assets/CHAR_ACTIVE_MOVE.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CHAR_ACTIVE_MOVE : MonoBehaviour {
+
+    public float speed;
+    private float vertAxis;
+    private float horAxis;
+    private CharacterController controller;
+    
+    
+
+    void Update()
+    {
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+            
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 90, 0);
+            transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+        }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 270, 0);
+            transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
+        }
+    }
+
+}
---
+++ Assets/CHAR_FRONT_DETECT.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CHAR_FRONT_DETECT : MonoBehaviour {
+
+    public GameObject detector;
+    public float detectDistance = 10;
+
+    void Update()
+    {
+        DetectForwardObject();
+    }
+    void DetectForwardObject()
+    {
+        RaycastHit objectHit;
+        Vector3 fwd = detector.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(detector.transform.position, fwd * detectDistance, Color.green);
+        if (Physics.Raycast(detector.transform.position, fwd, out objectHit, detectDistance))
+        {
+            if(objectHit.collider.tag == "Movable")
+            {
+                //pick up
+                Debug.Log("this is movable");
+            }
+            else if(objectHit.collider.tag == "Pressable")
+            {
+                //press
+                Debug.Log("This is pressable");
+            }
+                
+        }
+
+    }
+}
