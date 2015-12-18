01a5dee2c3d51aee5e92f8bfd2231b6bec92b9e2
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2015-12-18 12:16:45 -0500

    added new camera script

---
+++ Assets/CAMERA_OVERHEAD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CAMERA_OVERHEAD : MonoBehaviour {
+
+    public GameObject target;
+    public float damping;
+
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.transform.position;
+        Debug.Log(offset);
+	}
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = target.transform.position + offset;
+        Vector3 posision = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * damping);
+        transform.position = posision;
+    }
+	
+}
