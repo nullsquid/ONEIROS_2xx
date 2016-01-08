3641108434d438d3ab0d9dda08f73580fc5bf1bd
Azrael <j.shepard.ramsay@gmail.com>
2016-01-08 08:18:43 -0500

    Added initial module code

---
+++ Assets/MODULE.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MODULE : MonoBehaviour {
+
+    public bool isBinary;
+    public bool binInput;
+    public bool binOutput;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
