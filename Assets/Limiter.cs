12c4678cb2d10c9d613dca7d1b330de7f881f885
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2016-01-21 07:43:57 -0500

    added pips to limiter and started making the system that wil show the appropriate amount

--- Assets/Limiter.cs
+++ Assets/Limiter.cs
@@ -8,6 +8,12 @@ public class Limiter : MonoBehaviour {
     public int limiting = 0;
     public int outPower;
 
+    void Start()
+    {
+        CalculateOutPower(inPower);
+        //CheckPips(limiting);
+    }
+
     void OnEnable() {
         EventManager.StartListening("increaseCutoff", IncreaseCutoff);
     }
@@ -15,7 +21,34 @@ public class Limiter : MonoBehaviour {
         EventManager.StopListening("increaseCutoff", IncreaseCutoff);
     }
     void Update() {
-        
+        switch (limiting)
+        {
+            /*case 0:
+                CheckPips(limiting);
+                break;*/
+            case 1:
+                CheckPips(limiting);
+                break;
+            case 2:
+                CheckPips(limiting);
+                break;
+            case 3:
+                CheckPips(limiting);
+                break;
+        }
+    }
+    void CheckPips(int threshold)
+    {
+        foreach(Transform child in gameObject.GetComponent<Transform>())
+        {
+            
+        }
+
+        for(int i = threshold; i > 0; i--)
+        {
+            pips[i].GetComponent<Renderer>().enabled = false;
+        }
+
     }
 
     void CalculateOutPower(int input) {
@@ -27,5 +60,6 @@ public class Limiter : MonoBehaviour {
         if(limiting > inPower) {
             limiting = 0;
         }
+        CalculateOutPower(inPower);
     }
 }
